937c2ab5536772bebe37ce42f6917ab446e02413
guibibeau <g.bibeaulaviolette@gmail.com>
2025-02-23 13:24:08 -0500

    forfce redirect in prod to our page

--- apps/dapp/src/context/router.tsx
+++ apps/dapp/src/context/router.tsx
@@ -9,12 +9,22 @@ import { FaucetPage } from "pages/faucet-page";
 import { DeployTokenPage } from "pages/deploy-token-page";
 import { BridgePage } from "pages/bridge-page";
 import { PageBrancher } from "pages/page-brancher";
+import { environment } from "utils/environment";
 
 const router = createBrowserRouter([
   {
     path: "/*",
     element: <App />,
     errorElement: <ErrorPage />,
+    loader: async ({ request }) => {
+      if (new URL(request.url).pathname === "/" && environment.isProduction) {
+        return new Response(null, {
+          status: 302,
+          headers: { Location: "/84532/32" },
+        });
+      }
+      return null;
+    },
     children: [
       { path: "", element: <PageBrancher /> },
       { path: "faucet", element: <FaucetPage /> },
